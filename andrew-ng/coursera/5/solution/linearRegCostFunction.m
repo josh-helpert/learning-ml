25e32f3d9c646b6d45e4608959e4ec45c9dca72b
Josh Helpert <jhelpertmu@gmail.com>
2016-12-06 18:54:06 -0600

    Add cost and gradient with regularization of linear regression.

--- andrew-ng/coursera/5/solution/linearRegCostFunction.m
+++ andrew-ng/coursera/5/solution/linearRegCostFunction.m
@@ -17,18 +17,26 @@ grad = zeros(size(theta));
 %               regression for a particular choice of theta.
 %
 %               You should set J to the cost and grad to the gradient.
-%
 
+%X      12 x 2
+%y      12 x 1
+%theta  2  x 1
+%lambda 1  x 1
 
+% Hyp err term
+h = X * theta;
+err = h - y;
 
+% J regularization term
+theta_no_0 = [zeros(size(theta, 2)); theta(2:end, :)];
 
+J_reg = lambda * theta_no_0' * theta_no_0;
 
+% Cost
+J = (1 / (2 * m)) * (err' * err + J_reg);
 
-
-
-
-
-
+% Gradient
+grad = (1 / m) * (X' * err + lambda * theta_no_0);
 
 % =========================================================================
 
